4f8760e2c08124519ca5e0d7fc4773df930a9deb
Atarod61 <atarod2017@gmail.com>
2024-12-03 17:07:57 +0330

    ایجاد فایل

--- 9.go
+++ 9.go
@@ -1,7 +1,26 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"go/scanner"
+	"os"
+	"os/user"
+	"strconv"
+)
+
 func main() {
 
-	//var nameIsYou string = "Your name is here"
-	nameIsYou := "How are you"
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Enter somthing: ")
+	scanner.Scan()
+	userlnput := scanner.Text()
+	//Try to parse the user input as a number
+	if num, err := strconv.Atoi(userlnput): err == nil{
+		fmt.Printf("%d is an integer\n", num)
+	}else if -, err := strconv.ParseFloat(userIuserInput, 64); err == nil {
+		fmt.Printf("%s float\n", userIuserInput)
+	}else{
+		fmt.Printf("%s string\n", userIuserInput)
+	}
 }
