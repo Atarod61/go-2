0a23bfe74cd09c4bbdddca90f1be0005a418142a
Atarod61 <atarod2017@gmail.com>
2024-12-07 16:55:30 +0330

    fmt.Println(numbers)

--- 13.go
+++ 13.go
@@ -6,7 +6,14 @@ func main() {
 
 	numbers := []int{2, 3, 5, 11, 13, 1}
 	// 0, 1, 2, 3,  4, 5
-	fmt.Println("Last number:", numbers[len(numbers)-2])
+	fmt.Println(numbers)
+	numbers[0] = 6266
+	fmt.Println(numbers)
+
+	//fmt.Println(len(numbers[3]))
+
+	//Reverse Indexing
+	//fmt.Println("Last number:", numbers[len(numbers)-2])
 
 	//Length 1, 2, 3, 4, 5, 6
 	//fmt.Println(len(numbers))
