acf31be8a392bdb94e30c10866e79a5316839337
Atarod61 <atarod2017@gmail.com>
2024-12-07 16:42:32 +0330

    Improve feature/functionality”

--- 13.go
+++ 13.go
@@ -6,8 +6,10 @@ func main() {
 
 	numbers := []int{2, 3, 5, 11, 13, 1}
 	// 0, 1, 2, 3,  4, 5
+	fmt.Println("Last number:", numbers[len(numbers)])
+
 	//Length 1, 2, 3, 4, 5, 6
-	fmt.Println(len(numbers))
+	//fmt.Println(len(numbers))
 
 	//Indexing
 	//fmt.Println(numbers[2])
