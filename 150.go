ec7943e32031a1f041e68dae671787f18abaf2ec
Atarod61 <atarod2017@gmail.com>
2024-12-09 17:31:38 +0330

    Refactor code/feature”

--- 150.go
+++ 150.go
@@ -7,8 +7,8 @@ func main() {
 	originalNumbers := []int{2, 3, 5, 11, 13, 1, 12, 43, 65, 666, 192}
 	//                 0  1  2  3   4   5  6   7   8    9   10
 
-	for i123123 := range originalNumbers {
+	for index_number, index_value := range originalNumbers {
 
-		fmt.Println(i123123)
+		fmt.Println(index_number, "=", index_value)
 	}
 }
