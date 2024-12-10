0c7a401f61cc61b5b13c4f84ea821d5f8533ff9e
Atarod61 <atarod2017@gmail.com>
2024-12-10 12:13:30 +0330

    Refactor code/feature”

--- 152.go
+++ 152.go
@@ -4,11 +4,11 @@ import "fmt"
 
 func main() {
 
-	originalNumbers := []int{2, 3, 5, 11, 13, 1, 12, 43, 65, 666, 192}
+	originalNumbers := []string{"2", "3", "5", "11", "13", "1", "12", "43", "65", "666", "192"}
 	//                 0  1  2  3   4   5  6   7   8    9   10
 
 	for index_number, index_value := range originalNumbers {
 
-		fmt.Println(index_number, "=", index_value+1)
+		fmt.Println(index_number, "=", index_value+"**")
 	}
 }
