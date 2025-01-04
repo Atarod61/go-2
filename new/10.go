496314a562b8447a924496c0ca9dd5d39f882737
Atarod61 <atarod2017@gmail.com>
2025-01-04 19:25:04 +0330

    Add tests for feature

--- new/10.go
+++ new/10.go
@@ -4,7 +4,14 @@ import "fmt"
 
 func main() {
 
-	var a int = 10
-	var b int = 20
-	fmt.Println(a + b)
+	var myage int = 25
+	var myname string = "akram atarod"
+	var mysurname = "atarod"
+	var temp float64 = 26.5
+	//int = addad sahih
+	//string = jomleh ya kale
+	//float = addad ashari
+	var isprogrammer bool = true
+
+	fmt.Println(myage, myname, mysurname, temp, isprogrammer)
 }
