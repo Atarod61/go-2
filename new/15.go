4f4ab721c2ff504dcdc92dd81397b0f7dd62f84d
Atarod61 <atarod2017@gmail.com>
2025-01-13 20:06:17 +0330

    Add feature feature

--- new/15.go
+++ new/15.go
@@ -5,6 +5,9 @@ import "fmt"
 func main() {
 	var number int = 100
 	var mypointer *int = &number
-	fmt.Println("This is number address in memory", &number)
-	fmt.Println("This is mypointer address in memory", mypointer)
+	fmt.Println("This is number address in memory:", &number)
+	fmt.Println("This is mypointer address in memory:", mypointer)
+	fmt.Println(".........")
+	fmt.Println("This is number value:", number)
+	fmt.Println("This is mypointer value:", mypointer)
 }
