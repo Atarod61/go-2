0331259f2ecabe643ae810363b96910529d6c7b2
Atarod61 <atarod2017@gmail.com>
2025-01-13 20:41:02 +0330

    Add feature feature

--- new/151.go
+++ new/151.go
@@ -6,10 +6,10 @@ func main() {
 	var number int = 100
 	var mypointer *int = &number
 	fmt.Println(number)
-	sum(mypointe)
+	sum(mypointer)
 	fmt.Println(number)
 
 }
-func main(pointer *int) {
+func sum(pointer *int) {
 	*pointer = *pointer + 200
 }
