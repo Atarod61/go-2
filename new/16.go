e3d3b60197fe9ee11492452146fcf2d3d371e842
Atarod61 <atarod2017@gmail.com>
2025-01-15 13:05:47 +0330

    Add feature feature

--- new/16.go
+++ new/16.go
@@ -1,3 +1,24 @@
 package main
 
-func main
+import "fmt"
+
+func main() {
+	//const uri ="www.Toplearn.com"
+	//uri ="www.google.com"
+	const (
+		farvardin  = 1
+		ordibehesh = 2
+		khordad    = 3
+		tir        = 4
+		mordad     = 5
+		sharivar   = 6
+		mehr       = 7
+		aban       = 8
+		azar       = 9
+		dey        = 10
+		bahman     = 11
+		esfand     = 12
+	)
+	maheman = farvardin
+	fmt.Println(maheman)
+}
