ebbea5467c6a2176effb8ca780c170dc1d4d5fc9
Atarod61 <atarod2017@gmail.com>
2025-01-18 20:33:35 +0330

    Refactor code

--- new/17.go
+++ new/17.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
-	"unsafe"
 )
 
 func main() {
 
-	var A byte = 65 // مقدار ASCII کاراکتر 'A'
+	//var A byte = 65 // مقدار ASCII کاراکتر 'A'
 
-	fmt.Printf("value: %c, size: %d byte\n", A, unsafe.Sizeof(A))
+	//fmt.Printf("value: %c, size: %d byte\n", A, unsafe.Sizeof(A))
 
-	var Farsi rune = "پ"
-	fmt.Printf("value: %c, size: %d byte\n", Farsi, unsafe.Sizeof(Farsi))
+	//var Farsi rune = 'پ' // استفاده از rune('پ') برای تبدیل کاراکتر فارسی به rune
+	//fmt.Printf("value: %c, size: %d byte\n", Farsi, unsafe.Sizeof(Farsi))
+	srt := "Hello"
+	persiansrt := "سلام"
+	fmt.Println(len(srt))
+	fmt.Println(len(persiansrt))
 }
