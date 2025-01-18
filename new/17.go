a26c94c3a3ea9180ee63dc90e5d12022705b1fc0
Atarod61 <atarod2017@gmail.com>
2025-01-18 20:19:30 +0330

    Refactor code

--- new/17.go
+++ new/17.go
@@ -10,4 +10,7 @@ func main() {
 	var A byte = 65 // مقدار ASCII کاراکتر 'A'
 
 	fmt.Printf("value: %c, size: %d byte\n", A, unsafe.Sizeof(A))
+
+	var Farsi rune = "پ"
+	fmt.Printf("value: %c, size: %d byte\n", Farsi, unsafe.Sizeof(Farsi))
 }
