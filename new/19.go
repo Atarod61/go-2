c749f65e067354f05b8554a4662993c018d7be76
Atarod61 <atarod2017@gmail.com>
2025-01-21 10:52:37 +0330

    Refactor code

--- new/19.go
+++ new/19.go
@@ -8,6 +8,15 @@ func main() {
 	//var numbr int;
 	//number= 10
 	//...................
-	myvar := 10
-	fmt.Println(myvar)
+	//myvar := 10
+	//fmt.Println(myvar)
+	// myvar := 30
+	//fmt.Println(myvar)
+	//===============
+	var (
+		number   int    = 10
+		name     string = "reza"
+		isactive bool   = false
+	)
+	fmt.Println(number, name, isactive)
 }
