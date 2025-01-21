a90e7108f052a1cdd3d42217350bb28d56f7d18d
Atarod61 <atarod2017@gmail.com>
2025-01-21 11:50:57 +0330

    Refactor code

--- new/21.go
+++ new/21.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
-	fmt.Println(true)
-	fmt.Println(10)
-	fmt.Print("hello world")
-	fmt.Print(true)
-	fmt.Print(10)
+	//fmt.Println("hello world")
+	//fmt.Println(true)
+	//fmt.Println(10)
+	//fmt.Print("hello world")
+	//fmt.Print(true)
+	//fmt.Print(10)
+	var name string = "azam"
+	var age int = 27
+	fmt.Printf("my name is %s and my age is %d\n", name, age)
 }
