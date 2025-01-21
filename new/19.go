75a6bbce58cf6dc8cd0497952f3e7398cf6a7f11
Atarod61 <atarod2017@gmail.com>
2025-01-21 10:36:11 +0330

    add file

--- new/19.go
+++ new/19.go
@@ -3,22 +3,11 @@ package main
 import "fmt"
 
 func main() {
-
-	//intjer
-	var number1 int = 10
-	fmt.Println(number1)
-	var number2 float64 = 10.5
-	fmt.Println(number2)
-	//.......................
-	var srt string = "hello word"
-	var srt1 string = "سلام دنیا"
-	fmt.Println(len(srt))
-	fmt.Println(len([]rune(srt1)))
-	//......................
-	var bools bool = true
-	fmt.Println(bools)
-	//.....................
-	arr := [4]string{"ali", "reza", "mohammad", "yaser"}
-	fmt.Println(arr)
-
+	//var number int = 10
+	//....................
+	//var numbr int;
+	//number= 10
+	//...................
+	myvar := 10
+	fmt.Println(myvar)
 }
