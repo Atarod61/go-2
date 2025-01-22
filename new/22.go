a110455383f83ee6399cc1c1590f1735666ac58b
Atarod61 <atarod2017@gmail.com>
2025-01-22 08:51:38 +0330

    Refactor code

--- new/22.go
+++ new/22.go
@@ -5,8 +5,11 @@ import "fmt"
 func main() {
 
 	srt := "hello world"
-	//num := 10
-	//numfloat := 12.5
-	//bool := true
+	num := 10
+	numfloat := 12.5
+	bool := true
 	fmt.Printf("this is my string %s\n", srt)
+	fmt.Printf("this is my integer %d\n", num)
+	fmt.Printf("this is my float %f\n", numfloat)
+	fmt.Printf("this is my bool %t\n", bool)
 }
