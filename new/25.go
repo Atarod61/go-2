82cbf0a43d0f59fb4349732d8d6d89811b7cb2f5
Atarod61 <atarod2017@gmail.com>
2025-01-22 17:37:43 +0330

    Refactor code

--- new/25.go
+++ new/25.go
@@ -1,13 +1,23 @@
 package main
 
 func main(){
-	x:= 71
+	x:= 3
 	witch x {
-	case 1: fmt.Println("x eqals 1")
-	case 2: fmt.Println("x eqals 2")
-	case 3: fmt.Println("x eqals 3")
-	case 4:	fmt.Println("x eqals 4")
-	case 5: fmt.Println("x eqals 5")
-	default: fmt.Println("x does not eqals 1, 2, 3, 4, 5")
+	case 1: fmt.Println("sunday")
+	break
+	case 2: fmt.Println("Monday")
+	break
+	case 3: fmt.Println("Tuesday")
+	break
+	case 4:	fmt.Println("Wednesday")
+	break
+	case 5: fmt.Println("thursday")
+	break
+	case 5: fmt.Println("friday")
+	break
+	case 5: fmt.Println("saturday")
+	break
+	default: fmt.Println("we have worng")
 	}
+	Println("codes,,,,")
 }
