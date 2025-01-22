7b6d8508d1653924d3c3cc2f8a1d7a99d165c652
Atarod61 <atarod2017@gmail.com>
2025-01-22 16:51:53 +0330

    Refactor code

--- new/24.go
+++ new/24.go
@@ -1,14 +1,28 @@
 package main
 
+import "fmt"
+
 func main() {
 	//age1:= 17
-	age2 := 17
-	if age2 >= 18 {
-		println("yes! you can vote")
-	} else if age2 == 17 {
-		println("no! next year you can vote")
+	//age2 := 17
+	//if age2 >= 18 {
+	//println("yes! you can vote")
+	//} else if age2 == 17 {
+	//println("no! next year you can vote")
 
-	} else {
-		println("no!you can vote")
+	//} else {
+	//println("no!you can vote")
+	//}
+	num1 := 10
+	num2 := 20
+	if num1 <= num2 {
+		fmt.Println("num1 is less than or equal to num2")
 	}
+	if num1 > num2 {
+		fmt.Println("num1 is greater than num2")
+	}
+	if num1 == num2 {
+		fmt.Println("num1 is equal to num2")
+	}
+
 }
