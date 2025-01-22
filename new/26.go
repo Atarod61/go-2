92069b67ee3f494b0b1b7582267a1f07b46183cd
Atarod61 <atarod2017@gmail.com>
2025-01-22 20:02:34 +0330

    Refactor code

--- new/26.go
+++ new/26.go
@@ -3,8 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	for i := 1; i <= 10; i++ {
+	//for i := 1; i <= 10; i++ {
+	//fmt.Println(i)
+	//}
+	i := 10
+	for i <= 10 {
 		fmt.Println(i)
+		i++
 	}
 
 }
