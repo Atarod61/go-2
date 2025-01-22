13806ca8bfb6f8d51551e6eaec6a04ec7e647942
Atarod61 <atarod2017@gmail.com>
2025-01-22 16:40:27 +0330

    Refactor code

--- new/24.go
+++ new/24.go
@@ -5,8 +5,10 @@ func main() {
 	age2 := 17
 	if age2 >= 18 {
 		println("yes! you can vote")
-	}else age2==17 {
-		Println("no! next year you can vote")
+	} else if age2 == 17 {
+		println("no! next year you can vote")
 
+	} else {
+		println("no!you can vote")
 	}
 }
