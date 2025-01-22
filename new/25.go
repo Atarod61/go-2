2ce607ed9a10f4189ad6429d3ebf14f327af0ba5
Atarod61 <atarod2017@gmail.com>
2025-01-22 17:48:56 +0330

    Refactor code

--- new/25.go
+++ new/25.go
@@ -1,22 +1,41 @@
 package main
 
 func main(){
+	//x:= 3
+	//witch x {
+	//case 1: fmt.Println("sunday")
+	//break
+	//case 2: fmt.Println("Monday")
+	//break
+	//case 3: fmt.Println("Tuesday")
+	//break
+	//case 4:	fmt.Println("Wednesday")
+	//break
+	//case 5: fmt.Println("thursday")
+	//break
+	//case 5: fmt.Println("friday")
+	//break
+	//case 5: fmt.Println("saturday")
+	//break
+	//default: fmt.Println("we have worng")
+	//}
+	//Println("codes,,,,")
 	x:= 3
 	witch x {
 	case 1: fmt.Println("sunday")
-	break
+	
 	case 2: fmt.Println("Monday")
-	break
+	
 	case 3: fmt.Println("Tuesday")
-	break
+	fallthrough
 	case 4:	fmt.Println("Wednesday")
-	break
+
 	case 5: fmt.Println("thursday")
-	break
+	
 	case 5: fmt.Println("friday")
-	break
+	
 	case 5: fmt.Println("saturday")
-	break
+	
 	default: fmt.Println("we have worng")
 	}
 	Println("codes,,,,")
