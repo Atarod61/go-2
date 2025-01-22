c8ae801de4af7746c53837e4750f70e2bdb71549
Atarod61 <atarod2017@gmail.com>
2025-01-22 11:19:58 +0330

    Refactor code

--- new/23.go
+++ new/23.go
@@ -14,8 +14,10 @@ func main() {
 	//srt := "banana"
 	//subsrt := "a"
 	//fmt.Println(strings.Count(srt, subsrt))
+	//srt := "hello, world"
+	//fmt.Println(strings.HasPrefix(srt, "hello"))
+	//fmt.Println(strings.HasSuffix(srt, "world"))
 	srt := "hello, world"
-	fmt.Println(strings.HasPrefix(srt, "hello"))
-	fmt.Println(strings.HasSuffix(srt, "world"))
+	fmt.Println(strings.Index(srt, "world"))
 
 }
