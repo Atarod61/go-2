635becf3dbc36a0e237eb83f50df0ab14ddb99d7
Atarod61 <atarod2017@gmail.com>
2025-01-22 20:19:02 +0330

    Refactor code

--- new/26.go
+++ new/26.go
@@ -6,10 +6,24 @@ func main() {
 	//for i := 1; i <= 10; i++ {
 	//fmt.Println(i)
 	//}
+	//i := 1
+	//for i <= 10 {
+	//fmt.Println(i)
+	//i++
+	//}
+	//i:= 1
+	//for{
+	//fmt.Println(i)
+	//i++
+	//if i>10{
+	//break
+	//}
+	//}
 	i := 1
-	for i <= 10 {
+	for {
 		fmt.Println(i)
 		i++
+
 	}
 
 }
