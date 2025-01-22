605688b19df3fea71b3f2db0ff624f2394ecea38
Atarod61 <atarod2017@gmail.com>
2025-01-22 11:36:10 +0330

    Refactor code

--- new/23.go
+++ new/23.go
@@ -19,8 +19,10 @@ func main() {
 	//fmt.Println(strings.HasSuffix(srt, "world"))
 	//srt := "hello, world"
 	//fmt.Println(strings.Index(srt, "world"))
-	srt := []string{"azam", "atarod"}
-	srt2 := "*"
-	fmt.Println(strings.Join(srt, srt2))
-
+	//srt := []string{"azam", "atarod"}
+	//srt2 := "*"
+	//fmt.Println(strings.Join(srt, srt2))
+	srt := "a, b, c, d, e, f, g, h, i, i"
+	subsrt := ","
+	fmt.Println(strings.Split(srt, subsrt))
 }
