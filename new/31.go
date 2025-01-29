78963b544d06d23cc44ea6300740f4d4dbf4533e
Atarod61 <atarod2017@gmail.com>
2025-01-29 13:29:16 +0330

    add file

---
+++ new/31.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr1 := [5]int{1, 2, 3, 4, 5}
+	arr2 := arr1
+	arr2[0] = 100
+	arr2[1] = 200
+	arr2[2] = 300
+	fmt.Printf("this is arr1 :%d , arr2:%d\n", arr1, arr2)
+}
