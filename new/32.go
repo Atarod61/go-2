622c268b219e32e8c1e058f0a11e9644f4803513
Atarod61 <atarod2017@gmail.com>
2025-01-29 18:58:41 +0330

    Refactor code

--- new/32.go
+++ new/32.go
@@ -1,15 +1,15 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 	slices := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	newslice := make([]int, len(slices))
-	fmt.Printf("this is slice %d\n", newslice)
-	n := copy(newslice, slices)
-	println(n)
-	fmt.Printf("this is a slice %d", newslice)
+	//newslice := make([]int, len(slices))
+	//fmt.Printf("this is slice %d\n", newslice)
+	//n := copy(newslice, slices)
+	//println(n)
+	//fmt.Printf("this is a slice %d", newslice)
 
+}
+func myfunc(myslice []int) {
+	slice[0] = 10000
+	slice[2] = 2323233
 }
