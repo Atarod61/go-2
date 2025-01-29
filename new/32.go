965a566eeaa4631921b255b6691805240093fc09
Atarod61 <atarod2017@gmail.com>
2025-01-29 18:53:41 +0330

    Refactor code

--- new/32.go
+++ new/32.go
@@ -7,6 +7,9 @@ import (
 func main() {
 	slices := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	newslice := make([]int, len(slices))
-	fmt.Printf("this is slice %d", newslice)
+	fmt.Printf("this is slice %d\n", newslice)
+	n := copy(newslice, slices)
+	print(n)
+	fmt.Printf("this is a slice %d", newslice)
 
 }
