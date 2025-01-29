cfa1161e737b2ca50a67237080dfc2b320dd7e42
Atarod61 <atarod2017@gmail.com>
2025-01-29 18:46:26 +0330

    Refactor code

--- new/32.go
+++ new/32.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	slices := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	newslice := make([]int, len(slices))
+	fmt.Printf("this is slice %d", newslice)
 
 }
