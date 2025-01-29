e791076834b85b550884c7ce50952730b1f2affd
Atarod61 <atarod2017@gmail.com>
2025-01-29 18:28:24 +0330

    Refactor code

--- new/31.go
+++ new/31.go
@@ -3,11 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	arr1 := [5]int{1, 2, 3, 4, 5}
-	arr2 := arr1
-	arr2[0] = 100
-	arr2[1] = 200
-	arr2[2] = 300
-	fmt.Printf("this is arr1 :%d , arr2:%d\n", arr1, arr2)
-	print(&arr1, ",", &arr2)
+	//arr1 := [5]int{1, 2, 3, 4, 5}
+	//arr2 := arr1
+	//arr2[0] = 100
+	//arr2[1] = 200
+	//arr2[2] = 300
+	//fmt.Printf("this is arr1 :%d , arr2:%d\n", arr1, arr2)
+	//print(&arr1, ",", &arr2)
+	myslice1 := []int{1, 2, 3, 4, 5}
+	myslice2 := myslice1
+
+	myslice2[0] = 100
+	myslice2[2] = 300
+	myslice2[3] = 600
+	fmt.Printf("this is slice1 %d, slice2 %d\n", myslice1, myslice2)
 }
