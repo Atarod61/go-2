773d4d4d0ed7ff5a1a09d8f15864cbb297e536b0
Atarod61 <atarod2017@gmail.com>
2025-01-29 11:47:21 +0330

    Refactor code

--- new/30.go
+++ new/30.go
@@ -4,8 +4,11 @@ import "fmt"
 
 func main() {
 	myarray := [5]int{1, 2, 3, 4, 5}
-	myslice _ fullarray := myarray[:]
+	myslice- fullarray := myarray[:]
 
-	fmt.Println(myslice - fullarray)
+	fmt.Println(myslice _ fullarray)
+	myslice-PartofArray := myarray[1:3]
+	fmt.Println(myslice-PartofArray)
+
 
 }
