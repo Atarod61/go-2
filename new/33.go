5b9bfa5924066fd4377dc8bbadfef2eaf2c4cb4e
Atarod61 <atarod2017@gmail.com>
2025-02-04 20:11:41 +0330

    Refactor code

--- new/33.go
+++ new/33.go
@@ -1,10 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	slice1 := []int{1, 2, 3}
-	slice2 := []int{4, 5, 6}
-	slice1 = append(slice1, slice2...)
-	fmt.Println(slice1)
+	//slice1 := []int{1, 2, 3}
+	//slice2 := []int{4, 5, 6}
+	//slice1 = append(slice1, slice2...)
+	//fmt.Println(slice1)
+	numbers := []int{1, 2, 3}
+	part1 := numbers[:2]
+	part2afterappend := append([]int(10), numbers[2:]...)
+	atlast := append(part1, part2afterappend...)
+	fmt.Println(atlast)
 }
