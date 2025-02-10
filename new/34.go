7efbc6a7bdd0d627d4752c313149d47e30370ec4
Atarod61 <atarod2017@gmail.com>
2025-02-10 20:12:40 +0330

    Refactor code

--- new/34.go
+++ new/34.go
@@ -1,6 +1,10 @@
 package main
 
-func main() {
+import "fmt"
 
-mymap:
+func main() {
+	personmap := map[int]string{1234: "Ali emadi", 234567: "mohamad ranjbar"}
+	fmt.Println(personmap[1234])
+	fmt.Println(personmap[234567])
+
 }
