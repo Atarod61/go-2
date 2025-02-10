3485b306ad2ecbe6353f1975ed5b7df4123abd8d
Atarod61 <atarod2017@gmail.com>
2025-02-10 20:45:12 +0330

    Refactor code

--- new/35.go
+++ new/35.go
@@ -1,7 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 	mymap := map[int]string{12: "apple", 13: "orange", 14: "banana", 15: "pich"}
 	value, exists := mymap[12]
+	if exists {
+		fmt.Println("my key was", value)
+	} else {
+		fmt.Println("my key was not found")
+	}
 
 }
