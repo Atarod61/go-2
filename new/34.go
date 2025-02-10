c8070f10633b76cfee46f91c359d60e46ee9f844
Atarod61 <atarod2017@gmail.com>
2025-02-10 20:14:14 +0330

    Refactor code

--- new/34.go
+++ new/34.go
@@ -6,5 +6,6 @@ func main() {
 	personmap := map[int]string{1234: "Ali emadi", 234567: "mohamad ranjbar"}
 	fmt.Println(personmap[1234])
 	fmt.Println(personmap[234567])
+	fmt.Println(personmap[3456])
 
 }
