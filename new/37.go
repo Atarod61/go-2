86498805977fc8b696c72297557e429c49776878
Atarod61 <atarod2017@gmail.com>
2025-02-10 21:57:47 +0330

    Refactor code

--- new/37.go
+++ new/37.go
@@ -3,9 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	num1 := 10
-	num2 := 10
-	result := num1 + num2
+	//num1 := 10
+	//num2 := 10
+	//result := num1 + num2
+	//fmt.Println(result)
+	result := sum(10, 10)
 	fmt.Println(result)
 
+}
+func sum(number1 int, number2 int) int {
+	return numnumber1 + nunumber2
 }
