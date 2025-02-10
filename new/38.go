ab1e4d972f0d8907117ec7348e20639220c93b7a
Atarod61 <atarod2017@gmail.com>
2025-02-10 22:15:06 +0330

    Refactor code

--- new/38.go
+++ new/38.go
@@ -3,3 +3,16 @@ package main
 func main() {
 
 }
+func sum(a int, b int) int {
+	return a + b
+}
+func Minus(a int, b int) int {
+	return a - b
+}
+func Divided(a int, b int) float32 {
+	return float32(a / b)
+
+}
+func Multiplied(a int, b int) int {
+	return a * b
+}
