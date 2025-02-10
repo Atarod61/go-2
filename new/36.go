5423a6027f97c369f3d151b13997ae9b6537a010
Atarod61 <atarod2017@gmail.com>
2025-02-10 21:15:38 +0330

    Refactor code

--- new/36.go
+++ new/36.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	//sa := map[int]string{1: "alireza", 2: "mohamad", 3: "maryam", 4: "yashar", 5: "mostafa", 6: "massod"}
+
+	//fmt.Println(sa)
+	//delete(sa, 4)
+	//fmt.Println(sa)
 	sa := map[int]string{1: "alireza", 2: "mohamad", 3: "maryam", 4: "yashar", 5: "mostafa", 6: "massod"}
-
-	fmt.Println(sa)
-	delete(sa, 4)
-	fmt.Println(sa)
-
+	for key, value := range sa {
+		fmt.Printf("key: %d, value %s", key, value)
+	}
 }
