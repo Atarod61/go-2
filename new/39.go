829034f1d607f115905549b96a8eb812608f7fff
Atarod61 <atarod2017@gmail.com>
2025-02-11 11:14:28 +0330

    Refactor code

--- new/39.go
+++ new/39.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	r, b := divided(10, 3)
+	fmt.Println(r, b)
+
+}
+func Divided(a int, b int) (int, int) {
+	result := a / b
+	baghimande := a % b
+	return result, baghimande
 }
