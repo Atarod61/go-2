549f4baab6d28c756c7e60f768c0e2441007f107
Atarod61 <atarod2017@gmail.com>
2025-02-11 11:25:49 +0330

    Refactor code

--- new/39.go
+++ new/39.go
@@ -3,15 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	r, b := Divide(10, 3)
-	fmt.Println(r, b)
+	r, b, m := Divide(10, 3)
+	fmt.Println(r, b, m)
 
 }
 func Divide(a int, b int) (int, int, string) {
 	result := a / b
 	baghimande := a % b
 	var messsege string
-	if baghimande == 0 {
+	if baghimande != 0 {
 		messsege = "baghimande darad bakhsh pazir nist"
 	} else {
 		messsege = "baghimande nadarad bakhsh pazir hast!"
