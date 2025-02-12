9f7918c10e3356ca4a87e8c94a0371ef5343e147
Atarod61 <atarod2017@gmail.com>
2025-02-12 17:32:34 +0330

    Refactor code

--- new/func.go
+++ new/func.go
@@ -1,17 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
-
-
+	result := sum(10, 22, 34, 56)
+	fmt.Println(result)
 }
-func sum(nums...int)int{
-	total:=0
-	for- , num:= rangenums{
-		//total+=num
-
-	total= total+num
+func sum(nums ...int) int {
+	total := 0
+	for -,num := range nums {
+		total = total + num
 
 	}
-	
-	
+	return total
 }
