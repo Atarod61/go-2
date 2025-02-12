23f37c69153aee432e5f0e3f5fe44bee6f1451d4
Atarod61 <atarod2017@gmail.com>
2025-02-12 21:13:34 +0330

    Refactor code

--- new/42.go
+++ new/42.go
@@ -2,18 +2,33 @@ package main
 
 import "fmt"
 
-func main() {
-
+func counter() func() int {
 	count := 0
-	counter := func() int {
+	return func() int {
 		count++
 		return count
-
 	}
-	fmt.Println(counter())
-	fmt.Println(counter())
-	fmt.Println(counter())
-	fmt.Println(counter())
-	fmt.Println(counter())
-	fmt.Println(counter())
+}
+
+func main() {
+	result := counter()
+	fmt.Println(result())
+	fmt.Println(result())
+	fmt.Println(result())
+	fmt.Println(result())
+	fmt.Println(result())
+	fmt.Println(result())
+
+	//count := 0
+	//counter := func() int {
+	//count++
+	//return count
+
+	//}
+	//fmt.Println(counter())
+	//fmt.Println(counter())
+	//fmt.Println(counter())
+	//fmt.Println(counter())
+	//fmt.Println(counter())
+	//fmt.Println(counter())
 }
