064412041661f567cda714dac24cd639f85ed4eb
Atarod61 <atarod2017@gmail.com>
2025-02-12 17:10:18 +0330

    Refactor code

--- new/41.go
+++ new/41.go
@@ -1,6 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
+	myjam := compute(2, 2, jame)
+	mytafrigh := compute(10, 2, tafrigh)
+	fmt.Println(myjam)
+	fmt.Println(mytafrigh)
+
+}
+
+type opreation func(int, int) int
+
+func compute(a, b int, op opreation) int {
+	return op(a, b)
 
 }
 func jame(a, b int) int {
