2c6bdfc049841cb56544b2026bd27bfaa8938bd9
Atarod61 <atarod2017@gmail.com>
2025-02-23 17:37:58 +0330

    Refactor code

--- new/43.go
+++ new/43.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
 func main() {
@@ -10,12 +9,14 @@ func main() {
 	//defer fmt.Println("this is first")
 	//fmt.Println("this is second")
 	//defer fmt.Println("this is third")
-	file, err := os.Open("test.tex")
-	defer file.Close()
-	if err != nil {
-		fmt.Println("my err:", err)
-		return
-	}
-	fmt.Println("file opened:", file)
+	//file, err := os.Open("test.tex")
+	//	defer file.Close()
+	//if err != nil {
+	//fmt.Println("my err:", err)
+	//return
+	//}
+	//fmt.Println("file opened:", file)
+	x := 10
+	defer fmt.Println("this is whit defer", x)
 
 }
