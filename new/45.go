80d4fc1e786cf46b01920689acfcdb7dae8c0f2f
Atarod61 <atarod2017@gmail.com>
2025-02-24 11:12:00 +0330

    Refactor code

--- new/45.go
+++ new/45.go
@@ -9,22 +9,25 @@ type car struct {
 }
 
 func main() {
-	car1 := car{
-		name:       "Benz",
-		createDate: "2024",
-		company:    "Benz",
-	}
-	fmt.Println(car1)
-	car2 := &car{
-		name:       "Mercedes",
-		createDate: "2020",
-		company:    "Mercedes",
-	}
-	fmt.Println(car2)
-	carwithcomstracture := Newcar("paykan", "1898", "Irankhodro")
-	fmt.Println(carwithcomstracture)
+	//car1 := car{
+	//name:       "Benz",
+	//createDate: "2024",
+	//company:    "Benz",
+	//}
+	//fmt.Println(car1)
+	//car2 := &car{
+	//name:       "Mercedes",
+	//createDate: "2020",
+	//company:    "Mercedes",
+	//}
+	//fmt.Println(car2)
+	//carwithcomstracture := Newcar("paykan", "1898", "Irankhodro")
+	//fmt.Println(carwithcomstracture)
+	car4 := car{name: "Pride"}
+	fmt.Println(car4)
 
 }
-func Newcar(name string, createDate string, companyname string) *car {
-	return &car{name, createDate, companyname}
-}
+
+//func Newcar(name string, createDate string, companyname string) *car {
+//return &car{name, createDate, companyname}
+//}
