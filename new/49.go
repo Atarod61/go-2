1b9e8e9c01a20987af606d5080441226aa1dddac
Atarod61 <atarod2017@gmail.com>
2025-02-24 17:01:27 +0330

    Refactor code

--- new/49.go
+++ new/49.go
@@ -1,15 +1,38 @@
 package main
 
+import "fmt"
+
 type persons struct {
 	name string
 	Id   int
 	age  int
 }
-type Employ struct {
+type Employee struct {
 	code      string
 	TIMESTAMP int64
+	persons
 }
 
 func main() {
 
+	e := Employee{
+		code:      "12",
+		TIMESTAMP: 10,
+		persons: persons{
+			name: john,
+			Id:   1,
+			age:  25,
+		},
+	}
+	M := Maneger{
+		code:    "1234",
+		company: "cocompany",
+		persons: persons{
+			name: "max",
+			Id:   2,
+			age:  45,
+		},
+	}
+	fmt.Println(e)
+	fmt.Println(M)
 }
