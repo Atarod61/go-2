3d087dff70bde00c67a36a29836961c52062d2b9
Atarod61 <atarod2017@gmail.com>
2025-02-24 10:41:56 +0330

    Refactor code

--- new/45.go
+++ new/45.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
+type car struct {
+	name       string
+	createDate string
+	company    string
+}
+
 func main() {
+	car1 := car{
+		name:       "Benz",
+		createDate: "2024",
+		company:    "Benz",
+	}
+	fmt.Println(car1)
 
 }
