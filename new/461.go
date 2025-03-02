100973f44d4c6253bad141d0057b5b2a50464c1b
Atarod61 <atarod2017@gmail.com>
2025-03-02 11:01:23 +0330

    Refactor code

--- new/461.go
+++ new/461.go
@@ -1,10 +1,22 @@
 package main
 
+import "fmt"
+
 type Rectangle struct {
 	width  float64
 	height float64
 }
 
+func CaculateArea(width, height float64) float64 {
+	return width * height
+}
+func (r Rectangle) Displayinfo() string {
+	return fmt.Sprintf("Width:%2f, Height: %2f, Area: %2f", r.width, r.height, r.CaculateArea(r.width, r.height))
+}
+
 func main() {
+	r := Rectangle{width: 5, height: 3}
+	result := r.Displayinfo()
+	fmt.Println(result)
 
 }
