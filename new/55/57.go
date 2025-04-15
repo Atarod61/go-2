260ff6353edade343fe3ecafa4b5229aa35181a0
Atarod61 <atarod2017@gmail.com>
2025-04-15 12:53:32 +0430

    Refactor code

--- new/55/57.go
+++ new/55/57.go
@@ -1,11 +1,15 @@
 package main
 
+import "fmt"
+
 type MyError struct {
 	code    int
 	message string
 }
 
-func (e MyError) Error() string
+func (e MyError) Error() string {
+	fmt.Sprint("Error: %v\")
+}
 
 func main() {
 
