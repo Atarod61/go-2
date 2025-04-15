0b3a355235c0ce3f11c9c50240d1208c56f3ee8e
Atarod61 <atarod2017@gmail.com>
2025-04-15 12:39:36 +0430

    Refactor code

--- new/56.go
+++ new/56.go
@@ -1,13 +1,35 @@
 package main
 
-func main(){
-	type struct{
-		name string
-	age int
-	}{
-	name:"alireza"
-    age: 27,
-	
+import (
+	"errors"
+	"fmt"
+)
+
+//"encoding/json"
+//"fmt"
+
+func main() {
+	result, error := createError(true)
+	if error != nil {
+		fmt.Println(error)
+	}
+	fmt.Println(result)
+	//result := struct {
+	//name string
+	//age  int
+	//}{
+	//name: "alireza",
+	//age:  27,
+	//}
+	//studenjson, err := json.Marshal(result)
+	//if err != nil {
+	//fmt.Println("Error marshaling", err)
+	//}
+	//fmt.Println(string(studenjson))
 }
-	
+func createError(er bool) (string, error) {
+	if er {
+		return "", errors.New("error creating")
+	}
+	return "you dont like create error", nil
 }
