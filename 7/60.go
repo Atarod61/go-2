54d8484c5ba55fe4ee1582f17e015485cf3a5b23
Atarod61 <atarod2017@gmail.com>
2025-04-16 10:49:48 +0430

    Refactor code

--- 7/60.go
+++ 7/60.go
@@ -3,12 +3,13 @@ package main
 import "log"
 
 func main() {
-	isError := false
+	isError := true
 	if isError {
 		//log.Println("we have error here")
 		//log.Printf("your Error statues: %d", 404)
 		log.SetPrefix("ErrorMessage:")
 		log.SetFlags(log.Ldate | log.Ltime)
+		log.Printf("your Error statues: %d", 404)
 	} else {
 		log.Println("your program was started successfully")
 	}
